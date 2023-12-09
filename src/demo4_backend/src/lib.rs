542bba35a835caaef68bf4f448773e8e5e3602cd
Stanislav Golubtsov <sgaflv@gmail.com>
2023-12-09 17:17:25 +0100

    add demo files

---
+++ src/demo4_backend/src/lib.rs
@@ -0,0 +1,30 @@
+
+
+#[ic_cdk::query]
+fn eval(js_code: String) -> String {
+
+    use boa_engine::{Context, Source};
+    
+    let mut context = Context::default();
+    
+    match context.eval(Source::from_bytes(&js_code)) {
+        Ok(res) => {
+            
+            return res.to_string(&mut context).unwrap().to_std_string_escaped();
+        }
+        Err(e) => {
+            
+            eprintln!("Uncaught {e}");
+        }
+    };
+
+    return String::from("");
+}
+
+
+#[ic_cdk::init]
+fn init() {
+    ic_wasi_polyfill::init(&[0u8;32], &[]);
+}
+
+
